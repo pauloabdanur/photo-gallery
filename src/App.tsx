ce8536fb381b381602e9f083273eb1c23dc55731
Paulo Abdanur <ph_abdanur@yahoo.com.br>
2023-02-10 16:13:07 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, FormEvent } from 'react';
+import * as C from './App.styles';
+import * as Photos from './services/photos';
+import { Photo } from './types/Photo';
+import { PhotoItem } from './components/PhotoItem';
 
-function App() {
+const App = () => {
+  const [isUploading, setIsUploading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  const getPhotos = async () => {
+    setIsLoading(true);
+    setPhotos(await Photos.getAll());
+    setIsLoading(false);
+  };
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if (file && file.size > 0) {
+      setIsUploading(true);
+      let result = await Photos.uploadPhotos(file);
+      setIsUploading(false);
+
+      if (result instanceof Error) {
+        alert(`${result.name} - ${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
+  };
+
+  const handleDeletePhoto = async (name: string) => {
+    await Photos.deletePhoto(name);
+    getPhotos();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {/** Área de Upload */}
+        <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
+          <input type="file" name="image" />
+          <input type="submit" value="Enviar" />
+          {isUploading && 'Enviando...'}
+        </C.UploadForm>
+
+        {/** Lista de Fotos */}
+        {isLoading && (
+          <C.ScreenWarning>
+            <div className="emoji">✋</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        )}
+
+        {!isLoading && photos.length > 0 && (
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <PhotoItem
+                key={index}
+                url={item.url}
+                name={item.name}
+                onDelete={handleDeletePhoto}
+              />
+            ))}
+          </C.PhotoList>
+        )}
+
+        {!isLoading && photos.length === 0 && (
+          <C.ScreenWarning>
+            <div className="emoji">😔</div>
+            <div>Não há fotos cadastradas</div>
+          </C.ScreenWarning>
+        )}
+      </C.Area>
+    </C.Container>
   );
-}
+};
 
 export default App;
